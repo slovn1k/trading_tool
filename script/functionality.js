2a32baf0078bca73d6953f5129d2400f17abd1d3
Unknown <turcanualexandru1992@gmail.com>
2017-09-17 22:06:12 +0300

    First Commit
    Commit

---
+++ script/functionality.js
@@ -0,0 +1,132 @@
+var count_euro = 0, count_gbp = 0, count_usd = 0, count_cad = 0;
+var count_euro_div_up = 0, count_euro_div_down = 0,
+    count_gbp_div_up = 0, count_gbp_div_down = 0,
+    count_cad_div_up = 0, count_cad_div_down = 0,
+    count_usd_div_up = 0, count_usd_div_down = 0;
+
+$(document).click( function () {
+    if(count_euro == 3) {
+        $("#button_up_eur").hide();
+        $("#button_down_eur").hide();
+    }
+
+    function euro_color() {
+        if(count_euro_div_up == 3) {
+            $("#eur").addClass("money_up");
+        } else if(count_euro_div_down == 3) {
+            $("#eur").addClass("money_down");
+        }
+    }
+
+    euro_color();
+
+    if(count_gbp == 3) {
+        $("#button_up_gbp").hide();
+        $("#button_down_gbp").hide();
+    }
+
+    if(count_gbp_div_up == 3) {
+        $("#gbp").addClass("money_up");
+    } else if(count_gbp_div_down == 3) {
+        $("#gbp").addClass("money_down");
+    }
+
+    if(count_usd == 3) {
+        $("#button_up_usd").hide();
+        $("#button_down_usd").hide();
+    }
+
+    if(count_usd_div_up == 3) {
+        $("#usd").addClass("money_up");
+    } else if(count_usd_div_down == 3) {
+        $("#usd").addClass("money_down");
+    }
+
+    if(count_cad == 3) {
+        $("#button_up_cad").hide();
+        $("#button_down_cad").hide();
+    }
+
+    if(count_cad_div_up == 3) {
+        $("#cad").addClass("money_up");
+    } else if(count_cad_div_down == 3) {
+        $("#cad").addClass("money_down");
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    $(".arrow-up_eur").click(function () {
+
+        $(".arrow-up_eur").replaceWith("<div class='arrow-down_eur'></div>");
+        count_euro -= 1;
+        count_euro += 1;
+        count_euro_div_up -= 1;
+        count_euro_div_down += 1;
+
+    });
+
+});
+
+$("#reset_arrows").click( function () {
+    location.reload();
+});
+
+$("#button_up_eur").click( function () {
+    var $arrow_up_eur = $( "<div class='arrow-up_eur'></div>" );
+    $(".euro").append($arrow_up_eur);
+    count_euro += 1;
+    count_euro_div_up += 1;
+});
+
+$("#button_down_eur").click( function () {
+    var $arrow_down_eur = $( "<div class='arrow-down_eur'></div>" );
+    $(".euro").append($arrow_down_eur);
+    count_euro += 1;
+    count_euro_div_down += 1;
+});
+
+$("#button_up_gbp").click( function () {
+    var $arrow_up_gbp = $( "<div class='arrow-up_gbp'></div>" );
+    $(".pound").append($arrow_up_gbp);
+    count_gbp += 1;
+    count_gbp_div_up += 1;
+});
+
+$("#button_down_gbp").click( function () {
+    var $arrow_down_gbp = $( "<div class='arrow-down_gbp'></div>" );
+    $(".pound").append($arrow_down_gbp);
+    count_gbp += 1;
+    count_gbp_div_down += 1;
+});
+
+$("#button_up_usd").click( function () {
+    var $arrow_up_usd = $( "<div class='arrow-up_usd'></div>" );
+    $(".dollar").append($arrow_up_usd);
+    count_usd += 1;
+    count_usd_div_up += 1;
+});
+
+$("#button_down_usd").click( function () {
+    var $arrow_down_usd = $( "<div class='arrow-down_usd'></div>" );
+    $(".dollar").append($arrow_down_usd);
+    count_usd += 1;
+    count_usd_div_down += 1;
+});
+
+$("#button_up_cad").click( function () {
+    var $arrow_up_cad = $( "<div class='arrow-up_cad'></div>" );
+    $(".canada").append($arrow_up_cad);
+    count_cad += 1;
+    count_cad_div_up += 1;
+});
+
+$("#button_down_cad").click( function () {
+    var $arrow_down_cad = $( "<div class='arrow-down_cad'></div>" );
+    $(".canada").append($arrow_down_cad);
+    count_cad += 1;
+    count_cad_div_down += 1;
+});
+
+
+
+
